937ef154e939549eb3ecbe96a604e0738b05e001
riicardods <riicardoljn@icloud.com>
2023-12-03 22:08:14 -0300

    bank

--- TESTE/Program.cs
+++ TESTE/Program.cs
@@ -2,15 +2,46 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
 
-            for (int i = 1; i < 11; i++)
+
+            class ContaBancaria
+
+            // Atributos
+            public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        // Construtor
+        public ContaBancaria(string titular, double saldoInicial)
+        {
+            Titular = titular;
+            Saldo = saldoInicial;
+        }
+
+        // Método para realizar um depósito
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Depósito de {valor} realizado. Novo saldo: {Saldo}");
+        }
+
+        // Método para realizar um saque
+        public void Sacar(double valor)
+        {
+            if (valor <= Saldo)
             {
-                Console.WriteLine($"O numero é: {i}");
+                Saldo -= valor;
+                Console.WriteLine($"Saque de {valor} realizado. Novo saldo: {Saldo}");
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+            }
+        }
+
             }
 
-        }
 
     }
 }
