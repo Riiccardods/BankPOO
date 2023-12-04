682b82ff6da475206d11bc19c1c1d78c1d2c059e
riicardods <riicardoljn@icloud.com>
2023-12-03 22:02:51 -0300

    Adicionar arquivos de projeto.

---
+++ TESTE/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TESTE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"O numero é: {i}");
+            }
+        }
+    }
+}
